96a330b0953535e59f31046ed21db437fe56ba81
M4RK-1 <vizimark@gmail.com>
2024-03-10 18:51:54 +0100

    socket.io game placeholder,generalized file loading (server)

--- gamefield/main_page/server.js
+++ gamefield/main_page/server.js
@@ -4,26 +4,32 @@ const path = require('path');
 const socketIo = require('socket.io');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/chat.js') {
-        const filePath = path.join(__dirname, 'chat.js');
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'application/javascript',
+    };
+
+    const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code === 'ENOENT') {
+                fs.readFile(path.join(__dirname, 'index.html'), function(error, content) {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            } else {
+
                 res.writeHead(500);
-                return res.end('Error loading chat.js');
+                res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
             }
-            res.writeHead(200, { 'Content-Type': 'application/javascript' });
-            res.end(data);
-        });
-    } else {
-        fs.readFile('index.html', (err, data) => {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        });
-    }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
 });
 
 const io = socketIo(server);
@@ -37,10 +43,17 @@ io.on('connection', (socket) => {
         io.emit('chat message', message);
     });
 
+    socket.on('button clicked', () => {
+        const newText = "Clicked!";
+        io.emit('button change', newText);
+    });
+
     socket.on('disconnect', () => {
         userCount--;
         io.emit('user count', userCount);
     });
+
+
 });
 
 const PORT = process.env.PORT || 3000;
