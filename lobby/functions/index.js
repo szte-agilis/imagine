cc1bdcf991cbfb7bc9e2833845d45800d50ae0d1
tomitheninja <sudi.tamas@simonyi.bme.hu>
2024-03-11 14:01:33 +0100

    Lobby/Initialize firebase

---
+++ lobby/functions/index.js
@@ -0,0 +1,30 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const express = require("express");
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  logger.log("Hello from Firebase!");
+  res.send("Hello from Firebase!");
+});
+
+// handle errors
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+});
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+app.use("/lobby", router);
+exports.lobby = onRequest(app);
