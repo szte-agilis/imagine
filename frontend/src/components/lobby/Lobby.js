149f45d2c98b2e769ce96b1a3c74044f5dfa5767
Balázs Kis <bazsyhun@gmail.com>
2024-03-21 10:18:35 +0100

    folytattam a react implementálást, javítottam a lobby.js-t

--- frontend/src/components/lobby/Lobby.js
+++ frontend/src/components/lobby/Lobby.js
@@ -1,65 +1,63 @@
 import React from "react";
-import { signInAnonymously, getIdToken } from "firebase/auth";
-import { auth } from "../../lib/firebase-app";
+import { useState } from "react";
 import "./Lobby.css";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: "" };
+export default function App() {
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  const [username, setUsername] = useState("");
+  const [sessionId, setSessionId] = useState(0);
+
+  const handleUsernameChange = (event) => {
+    console.log(event);
+   setUsername(event.target.value);
+  }
+  const handleLobbyChange = (event) => {
+    setSessionId(event.target.value);
   }
 
-  handleChange(event) {
-    this.setState({ username: event.target.value });
-  }
 
-  handleSubmit = async (event) => {
-    event.preventDefault(); // Prevent default form submission
-    console.log("A name was submitted: " + this.state.username);
-    const credentials = await signInAnonymously(auth);
+  const handleSubmit = async (event) => {
+    //const credentials = await signInAnonymously(auth);
     // await updateCurrentUser(auth, { displayName: this.state.username });
-    console.log("User signed in anonymously with uid:", credentials.user.uid);
-    const token = await getIdToken(credentials.user);
+    //console.log("User signed in anonymously with uid:", credentials.user.uid);
+    //const token = await getIdToken(credentials.user);
+    localStorage.setItem(sessionId, username + '_' + sessionId);
+    window.location.href = "/gamefield";
 
-    await fetch("/lobby/join", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username: this.state.username, token }),
-    }).then(()=>{
-      window.location.href = "/gamefield";
-    });
+    // await fetch("/lobby/join", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ username: this.state.username }),
+    // }).then(()=>{
+    //   window.location.href = "/gamefield";
+    // });
   };
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
+  return (
+      <form onSubmit={()=>handleSubmit()}>
         <label htmlFor="username">Username</label>
         <input
           type="text"
           id="username"
           name="username"
-          value={this.state.username}
-          onChange={this.handleChange}
+          value={username}
+          onChange={(e)=>handleUsernameChange(e)}
+        />
+        <input
+          type="text"
+          id="lobby"
+          name="lobby"
+          value={sessionId}
+          onChange={(e)=>handleLobbyChange(e)}
         />
         <br />
-        <input
-          type="submit"
+        <button
+        type="submit"
           id="create-lobby"
           name="create-lobby"
-          value="create a lobby"
-        />
-        <br />
-        <input
-          type="submit"
-          id="join-lobby"
-          name="join-lobby"
-          value="join a lobby"
-        />
-      </form>
-    );
-  }
-}
+          onChange={(e)=>handleSubmit(e)}
+
+        >Csatlakozas</button>
+      </form> 
+)}
