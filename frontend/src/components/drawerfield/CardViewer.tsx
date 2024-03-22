232ac5f8811642d52aa104c005536912d3090f10
DinkeyKing <lanyivilmos@gmail.com>
2024-03-22 19:38:01 +0100

    socket probalkozas

--- frontend/src/components/drawerfield/CardViewer.tsx
+++ frontend/src/components/drawerfield/CardViewer.tsx
@@ -2,6 +2,7 @@ import Card from './Card';
 import { Vector2 } from '../../data/Vector2';
 import { CardTransform } from '../../data/CardTransform';
 import { MouseEvent, useState } from 'react';
+import { io } from 'socket.io-client';
 
 export default function CardViewer({ canDraw }: { canDraw: boolean }) {
     const pollFrequencyMs: number = 100;
@@ -10,22 +11,20 @@ export default function CardViewer({ canDraw }: { canDraw: boolean }) {
     let [cards, setCards] = useState([] as CardTransform[]);
     let [selectedIndex, setSelectedIndex] = useState(-1);
 
+    let socket = io()
+
     function addCard() {
         const card: CardTransform = new CardTransform(new Vector2(50, 100));
 
         setCards([...cards, card]);
-
-        /*
-
-        socket.emit('card-add', {card: card})
-
-        socket.on('card-add', function(card: CardTransform){
-            cards.push(card)
-            setCards([...cards])
-        })
-
-        */
+        
+        socket.emit('card-add', {card: card});
     }
+
+    socket.on('card-add', function(card: CardTransform){
+        setCards([...cards, card]);
+    })
+
 
     function onCardSelect(i: number) {
         if (!canDraw) return;
@@ -61,18 +60,14 @@ export default function CardViewer({ canDraw }: { canDraw: boolean }) {
             lastPoll = now;
             setLastPoll(lastPoll);
 
-            /*
-
             socket.emit('card-move', { index: selectedIndex, transform: cards[selectedIndex] });
-
-            socket.on('card-move', function(i: number, transform: CardTransform) {
-                cards[i] = transform;
-                setCards([...cards]);
-            });
-
-            */
         }
     }
+
+    socket.on('card-move', function(i: number, transform: CardTransform) {
+        cards[i] = transform;
+        setCards([...cards]);
+    });
 
     const cardList = cards.map((transform, i) => {
         return (
