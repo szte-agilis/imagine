eac5cae07741f6a63e2a19c461d36b36b7f922ca
SD <71507274+GamerFoxW-SD@users.noreply.github.com>
2024-03-24 23:42:32 +0100

    card id general and card move
    még nem igazi a mozgás de mukszik :D

--- frontend/src/components/drawerfield/Card.tsx
+++ frontend/src/components/drawerfield/Card.tsx
@@ -1,8 +1,19 @@
 import { CardTransform } from '../../data/CardTransform';
-import { MouseEventHandler, CSSProperties, MouseEvent } from 'react';
-import { images } from './imageImports';
+import { CSSProperties, useState } from 'react';
+import image from '../../assets/card.png';
 
-export default function Card({ transform, selectCallback}: { transform: CardTransform; selectCallback: MouseEventHandler }) {
+
+export default function Card({
+    transform,
+   
+    id,
+}: {
+    transform: CardTransform;
+   
+    id: any;
+}) {
+    const [isMouseDown, setIsMouseDown] = useState(false);
+
     let style: CSSProperties = {
         position: 'absolute',
         top: `${transform.position.y}px`,
@@ -12,15 +23,33 @@ export default function Card({ transform, selectCallback}: { transform: CardTran
         userSelect: 'none',
     };
 
-    const handleClick = (event: MouseEvent) => {
-        const { clientX, clientY } = event;
-        transform.position.x = clientX;
-        transform.position.y = clientY;
+    const handleMouseDown = () => {
+        setIsMouseDown(true);
+    };
+
+    const handleMouseUp = () => {
+        setIsMouseDown(false);
+    };
+
+    const handleMouseMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (isMouseDown) {
+            const { clientX, clientY } = event;
+            transform.position.x = clientX-50;
+            transform.position.y = clientY-80;
+            setIsMouseDown(false);
+        }
     };
 
     return (
-        <div draggable={true} style={style} onMouseDown={handleClick}>
-            <img alt="card" src={images.at(transform.image)} style={{ maxHeight: '160px' }} />
+        <div
+            id={id}
+            draggable={false} // Mivel saját mozgatást használunk, nem kell a DOM által kezelt húzhatóságot bekapcsolni
+            style={style}
+            onMouseDown={handleMouseDown}
+            //onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+        >
+            <img alt="card" src={image} style={{ maxHeight: '160px' }} />
         </div>
     );
 }
