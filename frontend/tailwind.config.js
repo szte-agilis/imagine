aed87e71aa638e90d07b81a88f3e6b1238e712ab
Rudnay Roland <h163369@stud.u-szeged.hu>
2024-03-24 14:20:57 +0100

    lobbyFrontendFixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src//*.{js,ts,jsx,tsx}'],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
     daisyui: {
-        themes: ['dim'],
+        themes: ['nord'],
     },
     plugins: [require('daisyui')],
 };
