dd9ac9ffc9297fbee3b4d966374e68d8b75d5233
Takács Balázs <takacsbalazs2014@gmail.com>
2024-04-05 19:40:56 +0200

    DrawerBoard.tsx comments

--- frontend/src/components/drawerfield/DrawerBoard.tsx
+++ frontend/src/components/drawerfield/DrawerBoard.tsx
@@ -1,31 +1,49 @@
-import {MouseEvent, useState, useEffect, MouseEventHandler} from 'react';
+import {MouseEvent, useState, useEffect} from 'react';
 import CardViewer from './CardViewer';
 import Deck from './Deck';
 import {CardTransform} from '../../data/CardTransform';
 import {Vector2} from '../../data/Vector2';
-import {io} from 'socket.io-client';
 import {images} from './imageImports';
+import {io} from 'socket.io-client';
 
-const pollFrequencyMs: number = 100;
+// the number of milliseconds to wait between card position updates
+// lower number -> faster updates, smoother movement, more network and CPU used
+const updateFrequencyMs: number = 100;
+
+// how close do we have to move the card to the edge of the board to remove it (in percentage)
 const margin: number = 1;
 
 export default function DrawerBoard() {
+    // the array storing the transforms of the currently placed cards
     let [cards, setCards] = useState([] as CardTransform[]);
-    let [lastPoll, setLastPoll] = useState(Date.now());
+
+    // the time of the last card movement update
+    let [lastUpdate, setLastUpdate] = useState(Date.now());
+
+    // is the deck currently open
     let [isDeckOpen, setIsDeckOpen] = useState(false);
+
+    // the index of the selected card in the cards array, -1 if no card is selected
     let [selectedIndex, setSelectedIndex] = useState(-1);
+
+    // TODO socket connection
     //let socket = io()
 
+    // add a listener to the window for the mouse up event
+    // need this for removing cards even when moved outside the board
+    // wrap it in useEffect ensuring it is added only once
     useEffect(() => {
         window.addEventListener('mouseup', putDownCard)
 
         return () => window.removeEventListener('mouseup', putDownCard);
     })
 
+    // pick up and start moving the card with the index 'i'
     function pickupCard(i: number) {
         setSelectedIndex(i);
     }
 
+    // put down the currently selected card
     function putDownCard(e: globalThis.MouseEvent) {
         if (selectedIndex < 0) return;
 
@@ -43,6 +61,7 @@ export default function DrawerBoard() {
         setSelectedIndex(-1);
     }
 
+    // move the selected card
     function moveCard(e: MouseEvent) {
         if (selectedIndex < 0) return;
 
@@ -59,17 +78,18 @@ export default function DrawerBoard() {
         setCards([...cards]);
 
         const now: number = Date.now();
-        const elapsed: number = now - lastPoll;
+        const elapsed: number = now - lastUpdate;
 
-        if (elapsed >= pollFrequencyMs) {
-            lastPoll = now;
-            setLastPoll(lastPoll);
+        if (elapsed >= updateFrequencyMs) {
+            lastUpdate = now;
+            setLastUpdate(lastUpdate);
 
             // TODO emit card move
             //socket.emit('card-move', { index: selectedIndex, transform: cards[selectedIndex] });
         }
     }
 
+    // add a card to the board from the deck
     function addCard(id: number) {
         const card: CardTransform = new CardTransform(id, new Vector2(50, 50), 0, 1);
 
@@ -77,11 +97,14 @@ export default function DrawerBoard() {
 
         setIsDeckOpen(false);
 
+        // TODO emit card add
         //socket.emit('card-add', {card: card});
     }
 
+    // the array of cards in the deck, that are all the cards currently not placed on the board
     const cardsInDeck: number[] = images.map((_, index) => index).filter(id => !cards.some(transform => transform.id === id));
 
+    // template
     return (
         <div id="board" className="h-full flex justify-center items-center relative border-4 border-slate-700" onMouseMove={moveCard}>
             <span className="absolute text-gray-400 select-none text-3xl z-10">Drawer board</span>
