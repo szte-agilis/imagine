909641770ce028aa743a72188abf80f95ecaaef0
Hegedus Peter Laszlo <h158522@stud.u-szeged.hu>
2024-04-12 16:52:55 +0200

    Revert "changed, using useEffect instead (#25)"
    This reverts commit 730e1548809f34eb361ecf432fe64a240c5ec4dd.

--- frontend/src/components/lobby/join-lobby/JoinLobby.jsx
+++ frontend/src/components/lobby/join-lobby/JoinLobby.jsx
@@ -27,14 +27,6 @@ export default function App() {
         return () => newSocket.close();
     }, []);
 
-    useEffect(() => {
-        const lobby = lobbies.find(lobby => lobby.id === sessionStorage.getItem("lobby"));
-        if (lobby && !lobby.gameStarted) {
-            sessionStorage.setItem("lobby", lobby.id)
-            window.location.href = "/lobby";
-        }
-    }, [lobbies]);
-
     function BackgroundImage() {
         const { src } = useImage({
             srcList: [bgImg, 'https://cdn.wallpapersafari.com/69/10/CEokAi.jpg'],
@@ -96,8 +88,18 @@ export default function App() {
     }
 
     function joinLobby(id) {
-        sessionStorage.setItem("lobby", id)
         socket.emit('list-lobbies');
+
+        socket.on('list-lobbies', (lobbies) => {
+            setLobbies(lobbies);
+
+            const lobby = lobbies.find(lobby => lobby.id === id);
+
+            if(!lobby.gameStarted){
+                sessionStorage.setItem("lobby", id)
+                window.location.href = "/lobby";
+            }
+        });
     }
 
     return (
