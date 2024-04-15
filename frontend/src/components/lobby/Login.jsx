3d89b0f75ad84699d0aa6f0f4912117b00a47d38
Eth1et <bodimartin22@gmail.com>
2024-04-15 16:59:29 +0200

    reszponzívvá tevés

--- frontend/src/components/lobby/Login.jsx
+++ frontend/src/components/lobby/Login.jsx
@@ -74,7 +74,7 @@ export default function App() {
                     backgroundImage: `url(${src})`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
-                    width: '25vw',
+                    width: '50vmin',
                     aspectRatio: 2.5
                 }}
             ></div>
@@ -115,8 +115,10 @@ export default function App() {
         }
 
         setShowWarning(false);
-        sessionStorage.setItem("username", username);
-        navigate('/join');
+        startTransition(() => {
+            sessionStorage.setItem("username", username);
+            navigate('/join');
+        });
     };
 
     const createLobby = async (event) => {
@@ -149,7 +151,7 @@ export default function App() {
             <div className="App relative">
                 {showWarning &&
                     <div id="warning" className="absolute w-full text-center py-4 lg:px-4 animated-warning">
-                        <div className="p-2 bg-red-100 items-center text-red-700 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+                        <div id="warning-msg" className="p-2 bg-red-100 items-center text-red-700 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
                             <span className="flex rounded-full bg-red-200 uppercase px-2 py-1 text-xs font-bold mr-3">Hoppá!</span>
                             <span className="font-semibold mr-2 text-left flex-auto text-red-500">{warningMessage}</span>
                             <svg id="warning-svg" onClick={hideWarning} className="cursor-pointer fill-current opacity-75 h-6 w-6 transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none max-w-[40px] max-h-[40px] text-xs hover:bg-gray-900/10 active:bg-gray-900/20 rounded-full " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
--- frontend/src/components/lobby/create-lobby/Lobby.jsx
+++ frontend/src/components/lobby/create-lobby/Lobby.jsx
@@ -62,7 +62,6 @@ export default function App() {
     });
 
     const handleBeforeUnload = useCallback((event) => {
-        //event.preventDefault();
         if(socket) {
             socket.emit('window closed', localLobby, localUsername);
         }
@@ -154,7 +153,7 @@ export default function App() {
                     backgroundImage: `url(${src})`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
-                    width: '25vw',
+                    width: '100%',
                     aspectRatio: 2.5
                 }}
             ></div>
@@ -165,7 +164,7 @@ export default function App() {
         return (
             <div id="player-list">
                 <div id="player-count-div" className="bg-gray-700">
-                    <p>Játékosok száma: {users.length}</p>
+                    <p>Játékosszám: {users.length}</p>
                 </div>
                 <div id="list-column" className="bg-gray-800">
                     {users.map((user, index) => (
@@ -254,7 +253,7 @@ export default function App() {
         <main>
             {showWarning && IsOwner(localUsername) &&
                 <div id="warning" className="absolute z-50 pointer-events-none w-full text-center py-4 lg:px-4 animated-warning">
-                    <div className="p-2 bg-red-100 items-center text-red-700 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+                    <div id='warning-msg' className="p-2 bg-red-100 items-center text-red-700 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
                         <span className="flex rounded-full bg-red-200 uppercase px-2 py-1 text-xs font-bold mr-3">Hoppá!</span>
                         <span className="font-semibold mr-2 text-left flex-auto text-red-500">{warningMessage}</span>
                         <svg id="warning-svg" onClick={hideWarning} className="cursor-pointer pointer-events-auto fill-current opacity-75 h-6 w-6 transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none max-w-[40px] max-h-[40px] text-xs hover:bg-gray-900/10 active:bg-gray-900/20 rounded-full " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
@@ -299,7 +298,7 @@ export default function App() {
                                     min={ROUNDS_MIN}
                                     max={ROUNDS_MAX}
                                 />
-                                <label for="rounds" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0]">Körök száma</label>
+                                <label for="rounds" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0]">Körszám</label>
                             </div>
                             <div class="relative z-0 w-full mb-5 group">
                                 <input type="number" name="roundTime" id="roundTime" disabled={!IsOwner(localUsername)} class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required
@@ -317,10 +316,10 @@ export default function App() {
                                     min={MAXPLAYERS_MIN}
                                     max={MAXPLAYERS_MAX}
                                 />
-                                <label for="maxPlayers" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0]">Játékos szám</label>
+                                <label for="maxPlayers" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0]">Játékosszám</label>
                             </div>
                         </div>
-                        <label for="category" class="block mb-2 text-sm font-medium text-gray-500 dark:text-white"><p id="category-p" className="text-gray-400">Kategória</p>
+                        <label for="category" class="block mb-2 text-sm font-medium text-gray-500 dark:text-white"><p id="category-p">Kategória</p>
                             <select id="category" name="category" disabled={!IsOwner(localUsername)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 value={lobbyData.category}
                                 onChange={handleFormChange}
