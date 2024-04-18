e1ed07e8a53f557c6de86704c65b8fca44385008
Takács Balázs <takacsbalazs2014@gmail.com>
2024-04-18 21:46:49 +0200

    do not render cards moved outside the board

--- frontend/src/components/drawerfield/CardViewer.tsx
+++ frontend/src/components/drawerfield/CardViewer.tsx
@@ -3,7 +3,7 @@ import { CardTransform } from '../../data/CardTransform';
 
 export default function CardViewer({ cards, selectedIndexes = [], selectCallback }: { cards: CardTransform[], selectedIndexes?: number[], selectCallback?: (i: number) => void }) {
     return (
-        <div className="size-full relative bg-gray-800">
+        <div className="size-full relative bg-gray-800 overflow-hidden">
             {cards.map((transform, index) => {
                 return (
                     <Card key={index} transform={transform} isSelected={selectedIndexes.includes(index)} selectCallback={() => selectCallback === undefined ? undefined : selectCallback(index)}/>
