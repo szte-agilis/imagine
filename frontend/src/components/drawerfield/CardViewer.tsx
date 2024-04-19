7868bb3ed734b0f8fc2e65ce05e242d3dad29472
Takács Balázs <takacsbalazs2014@gmail.com>
2024-04-19 19:59:27 +0200

    fixed board aspect ratio

--- frontend/src/components/drawerfield/CardViewer.tsx
+++ frontend/src/components/drawerfield/CardViewer.tsx
@@ -1,9 +1,11 @@
 import Card from './Card';
 import { CardTransform } from '../../data/CardTransform';
 
+export const ASPECT_RATIO: number = 16.0 / 9.0;
+
 export default function CardViewer({ cards, selectedIndexes = [], selectCallback }: { cards: CardTransform[], selectedIndexes?: number[], selectCallback?: (i: number) => void }) {
     return (
-        <div className="size-full relative bg-gray-800 overflow-hidden">
+        <div className="relative bg-gray-800 overflow-hidden" style={{aspectRatio: ASPECT_RATIO}}>
             {cards.map((transform, index) => {
                 return (
                     <Card key={index} transform={transform} isSelected={selectedIndexes.includes(index)} selectCallback={() => selectCallback === undefined ? undefined : selectCallback(index)}/>
