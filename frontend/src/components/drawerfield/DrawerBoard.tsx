acfc2d64741c6b15fae771ea2bd29866eec6b70c
Takács Balázs <takacsbalazsg@gmail.com>
2024-05-01 18:23:53 +0200

    comments and refactor

--- frontend/src/components/drawerfield/DrawerBoard.tsx
+++ frontend/src/components/drawerfield/DrawerBoard.tsx
@@ -15,12 +15,13 @@ const updateFrequencyMs: number = 100;
 const cardRemoveMargin: number = 1;
 
 export default function DrawerBoard({lobbyId, socket}: { lobbyId: string | null, socket: Socket | null }) {
-    // the array storing the transforms of the currently placed cards
+    // the state of the card array
     const [actualCards, setActualCards] = useState([] as CardTransform[]);
 
+    // the displayed state of the card array
     const [displayedCards, setDisplayedCards] = useState([] as CardTransform[]);
 
-    // array to store the indexes of the selected cards
+    // the indexes of the cards in the current selection
     const [selection, setSelection] = useState([] as number[]);
 
     // the time of the last card movement update
@@ -35,51 +36,54 @@ export default function DrawerBoard({lobbyId, socket}: { lobbyId: string | null,
     // is the left mouse button is pressed
     const [isMouseDown, setIsMouseDown] = useState(false);
 
-    // the board HTML element
+    // the board as an HTML element
     const board: HTMLElement = document.getElementById("board") as HTMLElement;
 
     // the array of cards in the deck (all the cards currently not placed on the board)
     const cardsInDeck: number[] = cardImages.map((_, index) => index).filter(id => !actualCards.some(transform => transform.id === id));
 
-    // add event listeners to the window
-    // wrap it in useEffect ensuring it is added only once
+    // handle keyboard and mouse events
     useEffect(() => {
         window.addEventListener('mousedown', onMouseDown);
         window.addEventListener('mouseup', onMouseUp);
-        window.addEventListener('mousemove', onCardMove);
         window.addEventListener('keyup', onKeyUp);
         window.addEventListener('keydown', onKeyDown);
+        window.addEventListener('mousemove', onMouseMove);
 
+        // must remove event listeners so they are not added multiple times
         return () => {
             window.removeEventListener('mousedown', onMouseDown);
             window.removeEventListener('mouseup', onMouseUp);
-            window.removeEventListener('mousemove', onCardMove);
             window.removeEventListener('keyup', onKeyUp);
             window.removeEventListener('keydown', onKeyDown);
+            window.removeEventListener('mousemove', onMouseMove);
         }
     });
 
     // handle mouse down event
-    function onMouseDown(_: MouseEvent) {
-        setIsMouseDown(true);
+    function onMouseDown(e: MouseEvent) {
+        // left mouse button is pressed
+        if (e.button === 0) {
+            setIsMouseDown(true);
+        }
     }
 
     // handle mouse up event
-    function onMouseUp(_: MouseEvent) {
-        // mouse button is no longer pressed
-        setIsMouseDown(false);
+    function onMouseUp(e: MouseEvent) {
+        // left mouse button released
+        if (e.button === 0) {
+            setIsMouseDown(false);
+        }
 
-        // nothing to do when no cards are selected
-        if(selection.length === 0){
+        // no cards are selected, do nothing
+        if (selection.length === 0) {
             return;
         }
 
         // do not reset selection when ctrl is pressed
         if (isCtrlDown) {
             // still stream updates so the board looks the same for everyone
-            if(socket){
-                socket.emit('card-modify', lobbyId, actualCards);
-            }
+            sendUpdates(true);
 
             return;
         }
@@ -96,27 +100,30 @@ export default function DrawerBoard({lobbyId, socket}: { lobbyId: string | null,
         setActualCards(remainingCards);
 
         // stream changes
-        if (socket) {
-            socket.emit('card-modify', lobbyId, remainingCards);
-        }
+        sendUpdates(true);
     }
 
     // handle key down events
     function onKeyDown(e: KeyboardEvent) {
         switch (e.key) {
-            case "ArrowLeft":
+            case 'ArrowLeft':
+                // rotate the selection to the left
                 handleRotate(-1);
                 break;
-            case "ArrowRight":
+            case 'ArrowRight':
+                // rotate the selection to the right
                 handleRotate(1);
                 break;
-            case "ArrowUp":
+            case 'ArrowUp':
+                // scale the selection up
                 handleScale(1.1);
                 break;
-            case "ArrowDown":
+            case 'ArrowDown':
+                // scale the selection down
                 handleScale(0.9);
                 break;
-            case "Control":
+            case 'Control':
+                // left control key is pressed
                 setIsCtrlDown(true);
                 break;
         }
@@ -124,13 +131,14 @@ export default function DrawerBoard({lobbyId, socket}: { lobbyId: string | null,
 
     // handle key up events
     function onKeyUp(e: KeyboardEvent) {
+        // left control key is released
         if (e.key === 'Control') {
             setIsCtrlDown(false);
         }
     }
 
     // handle mouse movement
-    function onCardMove(e: MouseEvent) {
+    function onMouseMove(e: MouseEvent) {
         // prevent default dragging behaviour
         e.preventDefault();
 
@@ -139,41 +147,56 @@ export default function DrawerBoard({lobbyId, socket}: { lobbyId: string | null,
             return;
         }
 
-        moveSelection(actualCards, selection, new Vector2(e.movementX, e.movementY), new Vector2(board.offsetWidth, board.offsetHeight));
+        // move the selection
+        handleMove(e.movementX, e.movementY);
+    }
 
-        const now: number = Date.now();
-        const elapsed: number = now - lastUpdate;
+    // add a card to the board
+    function handleAdd(id: number) {
+        const card: CardTransform = new CardTransform(id, new Vector2(50, 50));
 
-        if (elapsed >= updateFrequencyMs) {
-            setLastUpdate(now);
+        setActualCards([...actualCards, card]);
+        setIsDeckOpen(false);
 
-            if (socket) {
-                socket.emit('card-modify', lobbyId, actualCards);
-            }
+        if (socket) {
+            socket.emit('card-add', lobbyId, card);
         }
+    }
+
+    // move the selected cards
+    function handleMove(x: number, y: number) {
+        // move the selected cards
+        moveSelection(actualCards, selection, new Vector2(x, y), new Vector2(board.offsetWidth, board.offsetHeight));
+
+        // stream updates
+        sendUpdates();
 
         // update cards
         setActualCards([...actualCards]);
     }
 
-    function handleRotate(direction: number){
+    // rotate the selected cards
+    function handleRotate(direction: number) {
+        // rotation amount in degrees
         const amountDeg = 15 * direction;
 
+        // rotate the selection
         rotateSelection(actualCards, selection, amountDeg);
 
-        if(socket){
-            socket.emit('card-modify', lobbyId, actualCards);
-        }
+        // stream updates
+        sendUpdates();
 
+        // update cards
         setActualCards([...actualCards]);
     }
 
-    function handleScale(amount: number){
+    // scale the selected cards
+    function handleScale(amount: number) {
+        // scale the selection
         scaleSelection(actualCards, selection, amount);
 
-        if(socket){
-            socket.emit('card-modify', lobbyId, actualCards);
-        }
+        // stream updates
+        sendUpdates();
 
         setActualCards([...actualCards]);
     }
@@ -186,14 +209,12 @@ export default function DrawerBoard({lobbyId, socket}: { lobbyId: string | null,
 
             // not selected -> add to selection
             if (matchIndex < 0) {
-                selection.push(index);
-                setSelection([...selection]);
+                setSelection([...selection, index]);
             }
 
             // already selected -> remove from selection
             else {
-                selection.splice(matchIndex, 1);
-                setSelection([...selection]);
+                setSelection(selection.filter((_, i) => i !== matchIndex));
             }
         }
 
@@ -203,14 +224,19 @@ export default function DrawerBoard({lobbyId, socket}: { lobbyId: string | null,
         }
     }
 
-    // add a card to the board
-    function addCard(id: number) {
-        const card: CardTransform = new CardTransform(id, new Vector2(50, 50));
-        setActualCards([...actualCards, card]);
-        setIsDeckOpen(false);
+    // send updates to the lobby
+    function sendUpdates(force: boolean = false) {
+        // calculate elapsed time since last update
+        const now: number = Date.now();
+        const elapsed: number = now - lastUpdate;
 
-        if (socket) {
-            socket.emit('card-add', lobbyId, card);
+        // update the cards if enough time has passed
+        if (force || elapsed >= updateFrequencyMs) {
+            setLastUpdate(now);
+
+            if (socket) {
+                socket.emit('card-modify', lobbyId, actualCards);
+            }
         }
     }
 
@@ -226,7 +252,7 @@ export default function DrawerBoard({lobbyId, socket}: { lobbyId: string | null,
             </div>
 
             {isDeckOpen && <Deck
-                onCardSelect={addCard}
+                onCardSelect={handleAdd}
                 cardIds={cardsInDeck}
             />}
 
--- frontend/src/components/drawerfield/GuesserBoard.tsx
+++ frontend/src/components/drawerfield/GuesserBoard.tsx
@@ -1,40 +1,45 @@
-import CardViewer, { interpolateCardArray } from './CardViewer';
+import CardViewer, {interpolateCardArray} from './CardViewer';
 import {useEffect, useState} from 'react';
 import CardTransform from '../../data/CardTransform';
 import {Socket} from 'socket.io-client';
 import Interpolator from "./Interpolator";
 
 export default function GuesserBoard({socket}: { socket: Socket | null }) {
-    // the state of the cards
-    let [actualCards, setActualCards] = useState([] as CardTransform[]);
+    // the state of the card array
+    const [actualCards, setActualCards] = useState([] as CardTransform[]);
+
+    // the displayed state of the card array
     const [displayedCards, setDisplayedCards] = useState([] as CardTransform[]);
 
+    // handle socket events
     useEffect(() => {
-        if(socket){
-            socket.on('card-add', function(card: CardTransform){
-                actualCards.push(card);
-                setActualCards([...actualCards]);
-            })
-
-            socket.on('card-modify', function(transforms: CardTransform[]) {
-                actualCards = transforms;
-                setActualCards([...actualCards]);
-            });
-
-            socket.on('reset', function() {
-                actualCards = [];
-                setActualCards([...actualCards]);
-            });
+        // if the socket is not connected, do nothing
+        if (!socket) {
+            return;
         }
 
+        // add a single card to the board
+        socket.on('card-add', function (card: CardTransform) {
+            setActualCards([...actualCards, card]);
+        })
+
+        // refresh the board with the new card array
+        socket.on('card-modify', function (transforms: CardTransform[]) {
+            setActualCards(transforms);
+        });
+
+        // clear the board
+        socket.on('reset', function () {
+            setActualCards([]);
+        });
+
+        // must remove event listeners so they are not added multiple times
         return () => {
-            if (socket) {
-                socket.off('card-add');
-                socket.off('card-modify');
-                socket.off('reset');
-            }
+            socket.off('card-add');
+            socket.off('card-modify');
+            socket.off('reset');
         }
-    }, [socket]);
+    }, [socket, setActualCards, actualCards]);
 
     return (
         <div className="h-full flex flex-col relative border-4 border-t-0 border-sky-700">
