1cf29cc736acdff79313b244361b94536483e160
SD <71507274+GamerFoxW-SD@users.noreply.github.com>
2024-05-10 17:25:23 +0200

    jav

--- frontend/src/data/CardGrupsColors.ts
+++ frontend/src/data/CardGrupsColors.ts
@@ -1,5 +1,5 @@
 
-    function color(id:number, def_color:String ="#ffffff10"):String {
+    export function color(id:number, def_color:String ="#ffffff10"):String {
        const c=["#B8860B","#8B0000","#00008B","#006400"];
        let s=def_color;
        if(id>0){
@@ -9,7 +9,7 @@
         return s;
     }
 
-    function neon_color(id:number,def_color:String ="rgba(46,248,255,0.9)"):String{
+    export function neon_color(id:number,def_color:String ="rgba(46,248,255,0.9)"):String{
         const c=["rgba(255, 255, 0, 0.9)","rgba(255, 0, 0, 0.9)","rgba(0, 0, 255, 0.9)","rgba(0, 255, 0, 0.9)"];
         
         let s=def_color;
