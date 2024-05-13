9fbb06da00105afb89c887239cbd174b217f9c3b
SD <71507274+GamerFoxW-SD@users.noreply.github.com>
2024-05-13 12:53:26 +0200

    feher kijeloles szinezet kartyaknal

--- frontend/src/data/CardGrupsColors.ts
+++ frontend/src/data/CardGrupsColors.ts
@@ -10,12 +10,12 @@
     }
 
     export function neon_color(id:number,def_color:String ="rgba(46,248,255,0.9)"):String{
-        const c=["rgba(255, 255, 0, 0.9)","rgba(255, 0, 0, 0.9)","rgba(0, 0, 255, 0.9)","rgba(0, 255, 0, 0.9)"];
+        //const c=["rgba(255, 255, 0, 0.9)","rgba(255, 0, 0, 0.9)","rgba(0, 0, 255, 0.9)","rgba(0, 255, 0, 0.9)"];
         
         let s=def_color;
         
         if(id>0){
-            s=c[id-1];
+            s="rgba(255, 255, 255, 0.9)";
         }
         
         return s;
